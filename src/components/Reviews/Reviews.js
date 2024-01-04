11a45a1a1a36fcacce969908f59eae6f8b51b6a6
mainul96 <110577655+mainul96@users.noreply.github.com>
2024-01-04 21:23:01 +0530

    data show in table

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,20 +1,48 @@
 import React, { useEffect, useState } from 'react';
+import Table from 'react-bootstrap/Table';
 
 const Reviews = () => {
     const [reviews, setReviews] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch(`http://localhost:5000/reviews`)
-        .then(res=> res.json())
-        .then(data=>{
-           setReviews(data)
-        })
+            .then(res => res.json())
+            .then(data => {
+                setReviews(data)
+            })
     }, [])
 
     return (
-        <div>
-            <h1>Reviews Page:</h1>
-        </div>
+        <Table striped bordered hover className='mt-4'>
+            <thead>
+                <tr>
+
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>PhotoURL</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+            
+
+                    {
+                        reviews.map(review =>
+                            <tr>
+                                <td>{review?.name}</td>
+                                <td>{review?.email}</td>
+                                <td>{review?.photoURL}</td>
+                                <td>
+                                    <button className='btn btn-primary me-4'>Edit</button>
+                                    <button className='btn btn-warning'>Delete</button>
+                                </td>
+                            </tr>
+                        )
+
+
+                    }                                                                                                                                                                                                                                
+            </tbody>
+        </Table>
     );
 };
 
